44910ca8ff7bc26b8c9f672f6227973da1f1ac6c
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-07-08 13:54:15 +0300

    added api services

---
+++ src/services/api-services.js
@@ -0,0 +1,16 @@
+export default class ApiService {
+  constructor() {
+    this.API_KEY = 'R6T2f5StA43ZJAlAODPBSAJJjoAoGQks';
+    this.BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+    this.page = 1;
+  }
+  fetchImg(searchImg) {
+    const url = `${this.BASE_URL}&apikey=${this.API_KEY}`;
+    return fetch(url).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject('Opps! Something went wrong');
+    });
+  }
+}
