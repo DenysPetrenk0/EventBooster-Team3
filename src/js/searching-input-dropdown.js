01c5395ccf06341e0123bfa1fcc6925ef87359ee
Gupanets Dmytro <dmgpnts@gmail.com>
2021-07-11 19:10:40 +0300

    various fixes
    - Add object fit for event images
    - Add icons and its animation for search form

--- src/js/searching-input-dropdown.js
+++ src/js/searching-input-dropdown.js
@@ -8,8 +8,11 @@ const refs = {
   countryListRef: document.querySelector('.dropdown__list'),
   dropdownTitleRef: document.querySelector('.dropdown__title'),
   dropdownRef: document.querySelector('.dropdown'),
+  dropdownIconRef: document.querySelector('.dropdown__svg'),
   eventCardsRef: document.querySelector('.cards__list'),
   searchInputRef: document.querySelector('.form-field'),
+  searchIconRef: document.querySelector('.search__icon'),
+  clearSearchIconRef: document.querySelector('.clear-search__icon'),
 };
 
 const apiService = new ApiService();
@@ -32,13 +35,16 @@ function onClickDropdown(e) {
     }
 
     // Рендерим список стран
+
     refs.countryListRef.innerHTML = dropdownTpl(countryList);
 
     // Оперируем классом "visually-hidden" для скрытия списка стран по клику
     if (refs.countryListRef.classList.contains('visually-hidden')) {
       refs.countryListRef.classList.remove('visually-hidden');
+      refs.dropdownIconRef.classList.add('dropdown__svg--open');
     } else {
       refs.countryListRef.classList.add('visually-hidden');
+      refs.dropdownIconRef.classList.remove('dropdown__svg--open');
     }
   }
 
@@ -57,8 +63,18 @@ function onInputSearch(e) {
   apiService.keyword = e.target.value;
   const windowOuterWidth = window.outerWidth;
 
-  //для планшета (проверка по ширине браузера) меняем количество подгружаемых в запросе событий на 21.
-  if (windowOuterWidth > 768 && windowOuterWidth < 1280) apiService.size = 21;
+  if (!e.target.value.length) {
+    refs.searchIconRef.style.opacity = 1;
+    refs.clearSearchIconRef.style.opacity = 0;
+  }
+  if (e.target.value.length) {
+    refs.clearSearchIconRef.style.opacity = 1;
+    refs.searchIconRef.style.opacity = 0;
+  }
+
+  if (windowOuterWidth > 768 && windowOuterWidth < 1280)
+    //для планшета (проверка по ширине браузера) меняем количество подгружаемых в запросе событий на 21.
+    apiService.size = 21;
 
   apiService
     .fetchEvent()
